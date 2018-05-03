5d3937c84cc461d12e2047c0ebb748558e62dfe5
anatol-blackforest <anatolblackforest@gmail.com>
2018-05-03 21:05:09 +0300

    mongoose disconnect problem (TypeError: Cannot read property 'close' of undefined )

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-// const getTemperature = require('./modules/getTemperature');
-// const addTemperature = require('./modules/addTemperature');
-// const correctTemperature = require('./modules/correctTemperature');
-// const deleteTemperature = require('./modules/deleteTemperature');
-
 const router = express.Router();
 const Schema = mongoose.Schema;
 const url = "mongodb://localhost:27017/temperatures";
@@ -20,63 +15,67 @@ const Value = mongoose.model("Value", userScheme);
 
 let temperatures;
 
-const getTemperature = async () => {
-  await mongoose.connect(url);
-  temperatures = await Value.find();
-  mongoose.disconnect();
+const getTemperature = async res => {
+    try {
+        await mongoose.connect(url);
+        temperatures = await Value.find();
+        await mongoose.disconnect();
+        res.render('index', { title: 'Все показатели за сегодня', temperatures } )
+    } catch (err) {
+        console.log(err)
+        res.render('error', { err })
+    }
 }
 
-const addTemperature = async temperature => {
-  await mongoose.connect(url);
-  let value = new Value({
-      time: (new Date()).toLocaleString(),
-      temperature
-  });
-  await value.save();
-  mongoose.disconnect(); 
+const addTemperature = async (res, temperature) => {
+    try {
+        await mongoose.connect(url);
+        let value = new Value({
+            time: (new Date()).toLocaleString(),
+            temperature
+        });
+        await value.save();
+        await mongoose.disconnect(); 
+        res.redirect("/")
+    } catch (err) {
+        console.log(err)
+        res.render('error', { err })
+    }
 }
 
-const correctTemperature = async (id, temperature) => {
-    await mongoose.connect(url);
-    await Value.findByIdAndUpdate(id, {temperature})
-    mongoose.disconnect();
+const correctTemperature = async (res, id, temperature) => {
+    try {
+        await mongoose.connect(url);
+        await Value.findByIdAndUpdate(id, {temperature})
+        await mongoose.disconnect();
+        res.redirect("/")
+    } catch (err) {
+        console.log(err)
+        res.render('error', { err })
+    }
 }
 
-const deleteTemperature = async (id) => {
-    await mongoose.connect(url);
-    await Value.findByIdAndRemove(id)
-    mongoose.disconnect();
+const deleteTemperature = async (res, id) => {
+    try {
+        await mongoose.connect(url);
+        await Value.findByIdAndRemove(id)
+        await mongoose.disconnect();
+        res.redirect("/")
+    } catch (err) {
+        console.log(err)
+        res.render('error', { err })    
+    }
 }
 
 /* Вся температура за сутки. */
 router.get('/', (req, res) => {
-          getTemperature()
-          .then(() => res.render('index', { title: 'Все показатели за сегодня', temperatures } ))
-          .catch(err => {
-              mongoose.disconnect(); 
-              return res.render('error', { err })
-          })
+          getTemperature(res)
       }).post('/', (req, res) => {
-          addTemperature(parseFloat(req.body.val))
-          .then(() => res.redirect("/"))
-          .catch(err => {
-              mongoose.disconnect(); 
-              return res.render('error', { err })
-          })
+          addTemperature(res, parseFloat(req.body.val))
       }).put('/:id/:val', (req, res) => {
-         correctTemperature(req.params.id, parseFloat(req.params.val))
-          .then(() => res.redirect("/"))
-          .catch(err => {
-              mongoose.disconnect(); 
-              return res.render('error', { err })
-          })
+         correctTemperature(res, req.params.id, parseFloat(req.params.val))
       }).delete('/:id', (req, res) => {
-          deleteTemperature(req.params.id)
-          .then(() => res.redirect("/"))
-          .catch(err => {
-              mongoose.disconnect(); 
-              return res.render('error', { err })
-          })
+          deleteTemperature(res, req.params.id)
       })
 
 
